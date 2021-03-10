acee65fddee987985d2d005531e9397c398d29f1
Anita <a.nita@hotmail.co.uk>
2021-03-10 22:47:30 +0000

    remove for loop

--- index.js
+++ index.js
@@ -29,39 +29,18 @@ function statement(customer, movies) {
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
 
-  for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
-    let thisAmount = 0;
+  moviesPerCustomer(customer, movies)
+    .forEach((movie) => {
+      const price = getPricePerMovie(movie, movie.days);
 
-    // determine amount for each movie
-    switch (movie.code) {
-      case "regular":
-        thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        thisAmount = r.days * 3;
-        break;
-      case "childrens":
-        thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
-        }
-        break;
-    }
+      frequentRenterPoints++;
 
-    //add frequent renter points
-    frequentRenterPoints++;
+      if(movie.code === "new" && movie.days > 2) frequentRenterPoints++;
 
-    // add bonus for a two day new release rental
-    if(movie.code === "new" && r.days > 2) frequentRenterPoints++;
+      result += `\t${movie.title}\t${price}\n` ;
 
-    //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n` ;
-    totalAmount += thisAmount;
-  }
+      totalAmount += price;
+    });
 
   // add footer lines
   result += `Amount owed is ${totalAmount}\n`;
